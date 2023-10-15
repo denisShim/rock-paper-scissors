20b9e797645f92813515b044016103fffda66e7b
Denis Shimshirt <denis.shimshirt@gmail.com>
2023-10-15 15:38:43 +0300

    Made a function (game) with a counter of up to five victory points.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const playerInput = document.getElementById('playerInput');
 const button = document.querySelector('#button');
 const outPut = document.querySelector('#messege');
+const playerTurn = document.querySelector('.playerChoice');
+const computerTurn = document.querySelector('.computerChoice');
+
+let playerCount = 0;
+let computerCount = 0;
 
 function getComputerChoice(){
     const compSelect = ["камень", "ножницы", "бумага"];
@@ -8,48 +13,62 @@ function getComputerChoice(){
     return compSelect[randomNumber];
 }
 
-// let computerSelection = getComputerChoice();
-// let playerSelection = playerInput.value.toLowerCase();
 
 function playRound(playerSelection, computerSelection){
     
     if(computerSelection == playerSelection) {
         return outPut.textContent = "Ничья";
     } else if(computerSelection == "камень" && playerSelection == "ножницы"){
+        computerCount++;
         return outPut.textContent = "Копьютер победил!";
     } else if(playerSelection == "камень" && computerSelection == "ножницы"){
+        playerCount++;
         return outPut.textContent = "Человек победил!";
     } else if(computerSelection == "ножницы" && playerSelection == "бумага"){
+        computerCount++;
         return outPut.textContent = "Копьютер победил!";
     } else if(playerSelection == "ножницы" && computerSelection == "бумага"){
+        playerCount++;
         return outPut.textContent = "Человек победил";
     } else if(computerSelection == "бумага" && playerSelection == "камень"){
+        computerCount++;
         return outPut.textContent = "Копьютер победил!";
     } else if(playerSelection == "бумага" && computerSelection == "камень"){
+        playerCount++;
         return outPut.textContent = "Человек победил!";
     } else if(computerSelection == "ножницы" && playerSelection == "камень"){
+        playerCount++;
         return outPut.textContent = "Человек победил!";
     } else if(computerSelection == "камень" && playerSelection == "бумага"){
+        playerCount++;
         return outPut.textContent = "Человек победил!";
     } else {
         return outPut.textContent = "Нет совпадений!";
     }
 }
 
-button.addEventListener('click', function(){
+function game(){
     let computerSelection = getComputerChoice();
     let playerSelection = playerInput.value.toLowerCase();
     
-    console.log(playerSelection);
-    console.log(computerSelection);
+    playerTurn.textContent = "Твой ход - " + playerSelection;
+    computerTurn.textContent = "Компьтера ход - " + computerSelection;
+    
     playRound(playerSelection, computerSelection);
     playerInput.value = '';
-    
+
+    console.log(computerCount + " - comp");
+    console.log(playerCount + " - you")
+
+    if(playerCount === 5){
+        outPut.textContent = "ТВОЯ ПОБЕДА В ЭТОЙ ИГРЕ СО СЧЕТОМ " + playerCount + " - " + computerCount;
+        playerCount = 0;
+        computerCount = 0;
+    } else if(computerCount === 5){
+        outPut.textContent = "КОМПЬЮТЕРА ПОБЕДА В ЭТОЙ ИГРЕ СО СЧЕТОМ " + computerCount + " - " + playerCount;
+        playerCount = 0;
+        computerCount = 0;
+    }
 }
-)
 
-console.log(playerInput.value);
-
-
-
-// console.log(getComputerChoice());
+button.addEventListener('click', game);
